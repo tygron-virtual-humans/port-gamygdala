8b2f52a11469f8c0977d9a34202795343d9e5a7d
svenpopping <s.popping@student.tudelft.nl>
2015-04-29 10:40:56 +0200

    Test if travis and octopull work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Main.main");
+        System.out.println(getString());
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static String getString(){
+        return "Main.main";
     }
 }
