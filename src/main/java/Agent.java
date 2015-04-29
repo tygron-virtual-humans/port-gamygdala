deb99012154b1a1dd81f1ca5761ba9bac5a57c4f
Sven van Hal <S.R.P.vanHal@student.tudelft.nl>
2015-04-29 15:50:49 +0200

    Progress on Gamygdala and Agent classes.

--- src/main/java/Agent.java
+++ src/main/java/Agent.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Agent {
 
@@ -10,12 +11,16 @@ public class Agent {
 	/**
 	 * Collection of goals for this Agent.
 	 */
-	public ArrayList<Goal> goals;
+	public HashMap<String, Goal> goals;
 
 	/**
 	 * Collection of relations for this Agent.
 	 */
 	public ArrayList<Relation> currentRelations;
+
+	/**
+	 * Collection of emotions for this Agent.
+	 */
 	public ArrayList<Emotion> internalState;
 
 	/**
@@ -29,12 +34,46 @@ public class Agent {
 	private Gamygdala gamygdalaInstance;
 
 	/**
+	 * Pleasure Arousal Dominance mapping.
+	 */
+	private HashMap<String, double[]> mapPAD;
+
+	/**
 	 * Create new Agent.
 	 * 
 	 * @param name Agent name.
 	 */
 	public Agent(String name) {
 		this.name = name;
+
+		// Init goal map.
+		this.goals = new HashMap<String, Goal>();
+
+		// Init relation and emotion collections
+		this.currentRelations = new ArrayList<Relation>();
+		this.internalState = new ArrayList<Emotion>();
+
+		// Set gain
+		this.gain = 1;
+
+		// Initialize PAD map
+		this.mapPAD = new HashMap<String, double[]>(16, 1);
+		this.mapPAD.put("distress", new double[] { -0.61, 0.28, -0.36 });
+		this.mapPAD.put("fear", new double[] { -0.64, 0.6, -0.43 });
+		this.mapPAD.put("hope", new double[] { 0.51, 0.23, 0.14 });
+		this.mapPAD.put("joy", new double[] { 0.76, .48, 0.35 });
+		this.mapPAD.put("satisfaction", new double[] { 0.87, 0.2, 0.62 });
+		this.mapPAD.put("fear-confirmed", new double[] { -0.61, 0.06, -0.32 }); // defeated
+		this.mapPAD.put("disappointment", new double[] { -0.61, -0.15, -0.29 });
+		this.mapPAD.put("relief", new double[] { 0.29, -0.19, -0.28 });
+		this.mapPAD.put("happy-for", new double[] { 0.64, 0.35, 0.25 });
+		this.mapPAD.put("resentment", new double[] { -0.35, 0.35, 0.29 });
+		this.mapPAD.put("pity", new double[] { -0.52, 0.02, -0.21 }); // regretful
+		this.mapPAD.put("gloating", new double[] { -0.45, 0.48, 0.42 }); // cruel
+		this.mapPAD.put("gratitude", new double[] { 0.64, 0.16, -0.21 }); // grateful
+		this.mapPAD.put("anger", new double[] { -0.51, 0.59, 0.25 });
+		this.mapPAD.put("gratification", new double[] { 0.69, 0.57, 0.63 }); // triumphant
+		this.mapPAD.put("remorse", new double[] { -0.57, 0.28, -0.34 }); // guilty
 	}
 
 	/**
@@ -44,8 +83,10 @@ public class Agent {
 	 * @return True if goal was added successfully, false if not.
 	 */
 	public boolean addGoal(Goal g) {
-		if (g != null)
-			return this.goals.add(g);
+		if (g != null && !this.goals.containsKey(g.name)) {
+			this.goals.put(g.name, g);
+			return true;
+		}
 		return false;
 	}
 
@@ -56,7 +97,7 @@ public class Agent {
 	 * @return True if goal was removed successfully, false if not.
 	 */
 	public boolean removeGoal(Goal g) {
-		return this.goals.remove(g);
+		return this.goals.remove(g.name) != null;
 	}
 
 	/**
@@ -66,7 +107,7 @@ public class Agent {
 	 * @return True if Agent has goal, false if not.
 	 */
 	public boolean hasGoal(Goal g) {
-		return this.goals.contains(g);
+		return this.goals.containsKey(g.name);
 	}
 
 	public void getGoalByName() {
@@ -90,19 +131,107 @@ public class Agent {
 	public void printEmotionalState() {
 	}
 
-	public void updateRelation() {
+	/**
+	 * Sets the relation this agent has with the agent defined by agentName. If the relation does not exist, it will be created, otherwise it will be updated.
+	 * 
+	 * @param {String} agentName The agent who is the target of the relation.
+	 * @param {double} like The relation (between -1 and 1).
+	 */
+	public void updateRelation(String agentName, double like) {
+		if (!this.hasRelationWith(agentName)) {
+			// This relation does not exist, just add it.
+			this.currentRelations.add(new Relation(agentName, like));
+		} else {
+			// The relation already exists, update it.
+			Relation r;
+			for (int i = 0; i < this.currentRelations.size(); i++) {
+				r = this.currentRelations.get(i);
+				if (r.agentName == agentName) {
+					r.like = like;
+				}
+			}
+		}
 	}
 
-	public void hasRelationWith() {
+	/**
+	 * Checks if this agent has a relation with the agent defined by agentName.
+	 * 
+	 * @param {String} agentName The agent who is the target of the relation.
+	 * @param {boolean} True if the relation exists, otherwise false.
+	 */
+	public boolean hasRelationWith(String agentName) {
+		return (this.getRelation(agentName) != null);
 	}
 
-	public void getRelation() {
+	/**
+	 * Returns the relation object this agent has with the agent defined by agentName.
+	 * 
+	 * @param agentName The agent who is the target of the relation.
+	 * @param Relation The relation object or null if non existing.
+	 */
+	public Relation getRelation(String agentName) {
+		Relation r;
+		for (int i = 0; i < this.currentRelations.size(); i++) {
+			r = this.currentRelations.get(i);
+			if (r.agentName == agentName) {
+				return r;
+			}
+		}
+		return null;
 	}
 
-	public void printRelations() {
+	/**
+	 * Prints the relations this agent has with the agent defined by agentName.
+	 * 
+	 * @param agentName The agent who is the target of the relation. When omitted, all relations are printed.
+	 */
+	public void printRelations(String agentName) {
+		String output = this.name + " has the following sentiments:\n   ";
+		boolean found = false;
+
+		for (int i = 0; i < this.currentRelations.size(); i++) {
+
+			if (agentName == null || this.currentRelations.get(i).agentName == agentName) {
+				for (int j = 0; j < this.currentRelations.get(i).emotionList.size(); j++) {
+					output += this.currentRelations.get(i).emotionList.get(j).name + "(" + this.currentRelations.get(i).emotionList.get(j).intensity + ") ";
+					found = true;
+				}
+			}
+
+			output += " for " + this.currentRelations.get(i).agentName;
+
+			if (i < this.currentRelations.size() - 1) {
+				output += ", and\n   ";
+			}
+		}
+
+		if (found) {
+			System.out.println(output);
+		}
 	}
 
-	public void decay() {
+	/**
+	 * This method decays the emotional state and relations according to the decay factor and function defined in gamygdala.
+	 * Typically this is called automatically when you use startDecay() in Gamygdala, but you can use it yourself if you want to manage the timing.
+	 * This function is keeping track of the millis passed since the last call, and will (try to) keep the decay close to the desired decay factor, regardless the time passed
+	 * So you can call this any time you want (or, e.g., have the game loop call it, or have e.g., Phaser call it in the plugin update, which is default now).
+	 * Further, if you want to tweak the emotional intensity decay of individual agents, you should tweak the decayFactor per agent not the "frame rate" of the decay (as this doesn't change the rate).
+	 * 
+	 * @param gamygdalaInstance A reference to the correct Gamygdala instance that contains the decayFunction property to be used )(so you could use different gamygdala instances to manage different groups of agents)
+	 */
+	public void decay(Gamygdala gamygdalaInstance) {
+		for (int i = 0; i < this.internalState.size(); i++) {
+			double newIntensity = gamygdalaInstance.decayFunction.decay(this.internalState.get(i).intensity, gamygdalaInstance.getMillisPassed());
+			if (newIntensity < 0) {
+				this.internalState.remove(i);
+			} else {
+				this.internalState.get(i).intensity = newIntensity;
+			}
+		}
+
+		for (int i = 0; i < this.currentRelations.size(); i++) {
+			this.currentRelations.get(i).decay(gamygdalaInstance);
+		}
 	}
 
 	/**
--- src/main/java/Gamygdala.java
+++ src/main/java/Gamygdala.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * This is the main appraisal engine class taking care of interpreting a situation emotionally.
@@ -9,17 +10,17 @@ public class Gamygdala {
 	/**
 	 * Debug flag.
 	 */
-	public static final boolean debug = false;
+	public static final boolean debug = true;
 
 	/**
 	 * The collection of agents in this Gamygdala instance.
 	 */
-	public ArrayList<Agent> agents;
+	public HashMap<String, Agent> agents;
 
 	/**
 	 * The collection of goals in this Gamygdala instance.
 	 */
-	public ArrayList<Goal> goals;
+	public HashMap<String, Goal> goals;
 
 	/**
 	 * The decay function used to calculate emotion intensity.
@@ -34,24 +35,27 @@ public class Gamygdala {
 	/**
 	 * Timestamp of last emotion calculation.
 	 */
-	public long lastMillis;
+	private long lastMillis;
 
 	/**
 	 * Seconds since last emotion calculation.
 	 */
-	public long millisPassed;
+	private long millisPassed;
 
 	public Gamygdala() {
 
-		this.agents = new ArrayList<Agent>();
-		this.goals = new ArrayList<Goal>();
-		
+		// Init agent and goal maps
+		this.agents = new HashMap<String, Agent>();
+		this.goals = new HashMap<String, Goal>();
+
 		// Set default decay factor
 		this.decayFactor = .8;
-		
+
 		// Set default decay function
 		this.decayFunction = new LinearDecay(this.decayFactor);
 
+		// Record current time
+		this.lastMillis = System.currentTimeMillis();
 	}
 
 	/**
@@ -74,10 +78,140 @@ public class Gamygdala {
 	 */
 	public void registerAgent(Agent agent) {
 		agent.setGamygdalaInstance(this);
-		this.agents.add(agent);
+		this.agents.put(agent.name, agent);
 	}
-	
+
+	/**
+	 * A facilitator method to create a goal for a particular agent, that also registers the goal to the agent and Gamygdala.
+	 * This method is thus handy if you want to keep all Gamygdala logic internal to Gamygdala.
+	 * However, if you want to do more sophisticated stuff (e.g., goals for multiple agents, keep track of your own list of goals to also remove them, appraise events per agent without the need for Gamygdala to keep track of goals, etc...) this method will probably be doing too much.
+	 * 
+	 * @param agentName The agent's name to which the newly created goal has to be added.
+	 * @param goalName The goal's name.
+	 * @param goalUtility The goal's utility.
+	 * @return Goal - a goal reference to the newly created goal.
+	 */
+	public Goal createGoalForAgent(Agent agent, String goalName, double goalUtility) {
+		return this.createGoalForAgent(agent, goalName, goalUtility, false);
+	}
+
+	/**
+	 * A facilitator method to create a goal for a particular agent, that also registers the goal to the agent and Gamygdala.
+	 * This method is thus handy if you want to keep all Gamygdala logic internal to Gamygdala.
+	 * However, if you want to do more sophisticated stuff (e.g., goals for multiple agents, keep track of your own list of goals to also remove them, appraise events per agent without the need for Gamygdala to keep track of goals, etc...) this method will probably be doing too much.
+	 * 
+	 * @param agentName The agent's name to which the newly created goal has to be added.
+	 * @param goalName The goal's name.
+	 * @param goalUtility The goal's utility.
+	 * @return Goal - a goal reference to the newly created goal.
+	 */
+	public Goal createGoalForAgent(Agent agent, String goalName, double goalUtility, boolean isMaintenanceGoal) {
+
+		if (this.agents.containsKey(agent.name)) {
+			Goal goal = this.getGoalByName(goalName);
+
+			// Check if goal already exists
+			if (goal != null) {
+
+				if (isMaintenanceGoal) {
+					goal.isMaintenanceGoal = isMaintenanceGoal;
+				}
+
+				Gamygdala.debug("Warning: I cannot make a new goal with the same name " + goalName + " as one is registered already. I assume the goal is a common goal and will add the already known goal with that name to the agent " + agent);
+			} else {
+				// .. else, create new goal and add to this Gamygdala instance
+				goal = new Goal(goalName, goalUtility, isMaintenanceGoal);
+				this.registerGoal(goal);
+			}
+
+			// Register goal with agent
+			agent.addGoal(goal);
+
+			return goal;
+		} else {
+			Gamygdala.debug("Error: agent " + agent + " does not exist, so I cannot create a goal for it.");
+			return null;
+		}
+
+	}
+
+	/**
+	 * For every goal that NPC's or player characters can have you have to first create a Goal object and then register it using this method.
+	 * Registering the goals makes sure that Gamygdala will be able to find the correct goal references when a Beliefs about the game state comes in.
+	 * 
+	 * @param goal The goal to be registered.
+	 */
+	public void registerGoal(Goal goal) {
+		if (!this.goals.containsKey(goal.name))
+			this.goals.put(goal.name, goal);
+		else {
+			Gamygdala.debug("Warning: failed adding a second goal with the same name: " + goal.name);
+		}
+	}
+
+	/**
+	 * A facilitator method to create a relation between two agents. Both source and target have to exist and be registered with this Gamygdala instance.
+	 * This method is thus handy if you want to keep all gamygdala logic internal to Gamygdala.
+	 * 
+	 * @param sourceName The agent who has the relation (the source)
+	 * @param targetName The agent who is the target of the relation (the target)
+	 * @param relation The relation (between -1 and 1).
+	 */
+	public void createRelation(String sourceName, String targetName, double relation) {
+		Agent source = this.getAgentByName(sourceName);
+		Agent target = this.getAgentByName(targetName);
+		if (source != null && target != null && relation >= -1 && relation <= 1) {
+			source.updateRelation(targetName, relation);
+		} else {
+			debug("Error: cannot relate " + source + "  to " + target + " with intensity " + relation);
+		}
+	}
+
+	/**
+	 * Simple agent getter by name.
+	 * 
+	 * @param agentName The name of the agent to be found.
+	 * @return Agent null or an agent reference that has the name property equal to the agentName argument
+	 */
+	public Agent getAgentByName(String agentName) {
+		if (this.agents.containsKey(agentName)) {
+			return this.agents.get(agentName);
+		}
+		debug("Warning: agent \"" + agentName + "\" not found.");
+		return null;
+	}
+
+	/**
+	 * Simple goal getter by name.
+	 * 
+	 * @param goalName The name of the goal to be found.
+	 * @return Goal null or a goal reference that has the name property equal to the goalName argument.
+	 */
+	public Goal getGoalByName(String goalName) {
+		if (this.goals.containsKey(goalName)) {
+			return this.goals.get(goalName);
+		}
+		debug("Warning: goal \"" + goalName + "\" not found.");
+		return null;
+	}
+
+	/**
+	 * Get the amount of milliseconds that has passed since the last decay function was called.
+	 * 
+	 * @return long Milliseconds passed.
+	 */
 	public long getMillisPassed() {
 		return millisPassed;
+	}
+
+	/**
+	 * Print debug information to console if the debug flag is set to true.
+	 * 
+	 * @param what Object to print to console.
+	 */
+	public static void debug(Object what) {
+		if (Gamygdala.debug) {
+			System.out.println(what);
+		}
 	}
 }
