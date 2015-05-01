a5624806403485a448018eab9883903849ce32da
Sven van Hal <sven@svenvanhal.nl>
2015-05-01 13:46:08 +0200

    Create a new class to hold an Agent's emotions, thus with a single responsibility

--- src/main/java/Agent.java
+++ src/main/java/Agent.java
@@ -21,7 +21,7 @@ public class Agent {
   /**
    * Collection of emotions for this Agent.
    */
-  public ArrayList<Emotion> internalState;
+  public AgentInternalState internalState;
 
   /**
    * The gain for this agent. Must be between 0 and 20 inclusive.
@@ -53,7 +53,7 @@ public class Agent {
 
     // Init relation and emotion collections
     this.currentRelations = new ArrayList<Relation>();
-    this.internalState = new ArrayList<Emotion>();
+    this.internalState = new AgentInternalState();
 
     // Set gain
     this.gain = Agent.DEFAULT_GAIN;
@@ -61,7 +61,7 @@ public class Agent {
     // Initialize PAD map
     this.mapPad = new MapPad();
   }
-  
+
   /**
    * Add Goal.
    * 
@@ -140,61 +140,22 @@ public class Agent {
   }
 
   /**
-   * Updates the emotional state of this agent using a single emotion.
+   * Passthrough function to AgentInternalState.
    * 
    * @param emotion The emotion with which this Agent should be updated.
    */
   public void updateEmotionalState(Emotion emotion) {
-    Emotion temp;
-    for (int i = 0; i < this.internalState.size(); i++) {
-      temp = this.internalState.get(i);
-      if (temp.name == emotion.name) {
-        // Appraisals simply add to the old value of the emotion
-        // So repeated appraisals without decay will result in the sum of the appraisals over time
-        // To decay the emotional state, call .decay(decayFunction), or simply use the facilitating
-        // function in Gamygdala setDecay(timeMS).
-        temp.intensity += emotion.intensity;
-
-        // Test if this works without updating object, else:
-        // this.internalState.set(i, e);
-        return;
-      }
-    }
-
-    // copy on keep, we need to maintain a list of current emotions for the state, not a list
-    // references to the appraisal engine
-    this.internalState.add(new Emotion(emotion.name, emotion.intensity));
+    this.internalState.updateEmotionalState(emotion);
   }
 
   /**
-   * This function returns either the state as is (gain=false) or a state based on gained limiter
-   * (limited between 0 and 1), of which the gain can be set by using setGain(gain).
-   * A high gain factor works well when appraisals are small and rare, and you want to see the
-   * effect of these appraisals
-   * A low gain factor (close to 0 but in any case below 1) works well for high frequency and/or
-   * large appraisals, so that the effect of these is dampened.
+   * Passthrough function to AgentInternalState.
    * 
-   * @param useGain Whether to use the gain function or not.
+   * @param useGain The gain factor. Leave blank (null) to ignore gain.
    * @return An array of emotions.
    */
-  public ArrayList<Emotion> getEmotionalState(boolean useGain) {
-
-    if (useGain) {
-      ArrayList<Emotion> gainState = new ArrayList<Emotion>();
-      Emotion emotion;
-      for (int i = 0; i < this.internalState.size(); i++) {
-        emotion = this.internalState.get(i);
-
-        if (emotion != null) {
-          double gainEmo = (this.gain * emotion.intensity) / (this.gain * emotion.intensity + 1);
-          gainState.add(new Emotion(emotion.name, gainEmo));
-        }
-      }
-
-      return gainState;
-    }
-
-    return this.internalState;
+  public AgentInternalState getEmotionalState(Double gain) {
+    return this.internalState.getEmotionalState(gain);
   }
 
   /**
@@ -205,18 +166,11 @@ public class Agent {
    * A low gain factor (close to 0 but in any case below 1) works well for high frequency and/or
    * large appraisals, so that the effect of these is dampened.
    * 
-   * @param useGain Whether to use the gain function or not.
+   * @param gain The gain factor. Leave blank (null) to ignore gain.
    */
-  public void printEmotionalState(boolean useGain) {
+  public void printEmotionalState(Double gain) {
     String output = this.name + " feels ";
-
-    ArrayList<Emotion> emotionalState = this.getEmotionalState(useGain);
-
-    Emotion emotion;
-    for (int i = 0; i < emotionalState.size(); i++) {
-      emotion = emotionalState.get(i);
-      output += emotion.name + ": " + emotion.intensity + ", ";
-    }
+    output += this.internalState.getEmotionalState(gain);
 
     System.out.println(output);
   }
---
+++ src/main/java/AgentInternalState.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+
+/**
+ * The internal state of an Agent. Contains all it's emotions.
+ */
+public class AgentInternalState extends ArrayList<Emotion> {
+
+  /**
+   * ID for serialization.
+   */
+  private static final long serialVersionUID = 2896244475677504657L;
+
+  /**
+   * Updates the emotional state of this agent using a single emotion.
+   * 
+   * @param emotion The emotion with which this Agent should be updated.
+   */
+  public void updateEmotionalState(Emotion emotion) {
+    Emotion temp;
+    for (int i = 0; i < size(); i++) {
+      temp = get(i);
+      if (temp.name == emotion.name) {
+        // Appraisals simply add to the old value of the emotion
+        // So repeated appraisals without decay will result in the sum of the appraisals over time
+        // To decay the emotional state, call .decay(decayFunction), or simply use the facilitating
+        // function in Gamygdala setDecay(timeMS).
+        temp.intensity += emotion.intensity;
+
+        // Test if this works without updating object, else:
+        // this.internalState.set(i, e);
+        return;
+      }
+    }
+
+    // copy on keep, we need to maintain a list of current emotions for the state, not a list
+    // references to the appraisal engine
+    add(new Emotion(emotion.name, emotion.intensity));
+  }
+
+  /**
+   * This function returns either the state as is (gain=false) or a state based on gained limiter
+   * (limited between 0 and 1), of which the gain can be set by using setGain(gain).
+   * A high gain factor works well when appraisals are small and rare, and you want to see the
+   * effect of these appraisals
+   * A low gain factor (close to 0 but in any case below 1) works well for high frequency and/or
+   * large appraisals, so that the effect of these is dampened.
+   * 
+   * @param gain The gain factor. Leave blank (null) to ignore gain.
+   * @return An array of emotions.
+   */
+  public AgentInternalState getEmotionalState(Double gain) {
+
+    if (gain != null) {
+      AgentInternalState gainState = new AgentInternalState();
+      Emotion emotion;
+      for (int i = 0; i < size(); i++) {
+        emotion = get(i);
+
+        if (emotion != null) {
+          double gainEmo = (gain * emotion.intensity) / (gain * emotion.intensity + 1);
+          gainState.add(new Emotion(emotion.name, gainEmo));
+        }
+      }
+
+      return gainState;
+    }
+
+    return this;
+  }
+
+  /**
+   * This function prints to the console either the state as is (gain=false) or a state based on
+   * gained limiter (limited between 0 and 1), of which the gain can be set by using setGain(gain).
+   * A high gain factor works well when appraisals are small and rare, and you want to see the
+   * effect of these appraisals
+   * A low gain factor (close to 0 but in any case below 1) works well for high frequency and/or
+   * large appraisals, so that the effect of these is dampened.
+   * 
+   * @param gain The gain factor. Leave blank (null) to ignore gain.
+   */
+  public String printEmotionalState(Double gain) {
+
+    String output = "";
+
+    AgentInternalState emotionalState = this.getEmotionalState(gain);
+
+    Emotion emotion;
+    for (int i = 0; i < emotionalState.size(); i++) {
+      emotion = emotionalState.get(i);
+      output += emotion.name + ": " + emotion.intensity + ", ";
+    }
+
+    return output;
+  }
+
+}
