32c6bfb3977cd5042b4f8267ae334712da5e9721
nordinvannes <N.vanNes@student.tudelft.nl>
2015-05-08 10:53:12 +0200

    ExponentialDecay.java javadoc added

--- src/main/java/decayfunction/ExponentialDecay.java
+++ src/main/java/decayfunction/ExponentialDecay.java
@@ -1,11 +1,24 @@
 package decayfunction;
 
+/**
+ * ExponentialDecay Class can calculate exponential decay function.
+ */
 public class ExponentialDecay extends DecayFunction {
 
+    /**
+     * Constructor.
+     * @param decayFactor The decay factor.
+     */
     public ExponentialDecay(double decayFactor) {
         super(decayFactor);
     }
 
+    /**
+     * Returns the decay outcome.
+     * @param initial      Initial emotion intensity.
+     * @param millisPassed Milliseconds since last decay function execution.
+     * @return
+     */
     @Override
     public double decay(double initial, long millisPassed) {
         return initial * Math.pow(getDecayFactor(), millisPassed / 1000);
