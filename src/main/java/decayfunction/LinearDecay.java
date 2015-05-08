6eb802b346b29ee0ebd309129ca060c79c6838db
nordinvannes <N.vanNes@student.tudelft.nl>
2015-05-08 10:56:42 +0200

    LinearDecay.java javadoc added

--- src/main/java/decayfunction/LinearDecay.java
+++ src/main/java/decayfunction/LinearDecay.java
@@ -1,11 +1,24 @@
 package decayfunction;
 
+/**
+ * LinearDecay class can calculate the decay by a linear model.
+ */
 public class LinearDecay extends DecayFunction {
 
+    /**
+     * Constructor.
+     * @param decayFactor The decay factor.
+     */
     public LinearDecay(double decayFactor) {
         super(decayFactor);
     }
 
+    /**
+     * Returns the decay by linear model.
+     * @param initial      Initial emotion intensity.
+     * @param millisPassed Milliseconds since last decay function execution.
+     * @return the decay by linear model.
+     */
     @Override
     public double decay(double initial, long millisPassed) {
         return initial - getDecayFactor() * (millisPassed / 1000);
