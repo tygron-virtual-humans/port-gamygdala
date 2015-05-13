bac4d1cab3c7f3a5614e2fc27bb75c9b2faf5a85
Sven van Hal <sven@svenvanhal.nl>
2015-05-13 14:03:34 +0200

    Update JavaDoc of AgentInternalState.

--- src/main/java/agent/AgentInternalState.java
+++ src/main/java/agent/AgentInternalState.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import data.Emotion;
 
-
 /**
  * The internal state of an Agent. Contains all it's emotions.
  */
@@ -24,31 +23,32 @@ public class AgentInternalState extends ArrayList<Emotion> {
         Emotion temp;
         for (int i = 0; i < size(); i++) {
             temp = get(i);
+
             if (temp.name == emotion.name) {
-                // Appraisals simply add to the old value of the emotion
-                // So repeated appraisals without decay will result in the sum of the appraisals over time
-                // To decay the emotional state, call .decay(decayFunction), or simply use the facilitating
-                // function in Gamygdala setDecay(timeMS).
+
+                // Appraisals simply add to the old value of the emotion.
+                // Repeated appraisals without decay will result in the sum of
+                // the appraisals over time. To decay the emotional state, call
+                // Gamygdala.decay(decayFunction).
                 temp.intensity += emotion.intensity;
 
-                // Test if this works without updating object, else:
-                // this.internalState.set(i, e);
                 return;
             }
         }
 
-        // copy on keep, we need to maintain a list of current emotions for the state, not a list
-        // references to the appraisal engine
+        // copy on keep, we need to maintain a list of current emotions for the
+        // state, not a list references to the appraisal engine
         add(new Emotion(emotion.name, emotion.intensity));
     }
 
     /**
-     * This function returns either the state as is (gain=false) or a state based on gained limiter
-     * (limited between 0 and 1), of which the gain can be set by using setGain(gain).
-     * A high gain factor works well when appraisals are small and rare, and you want to see the
-     * effect of these appraisals
-     * A low gain factor (close to 0 but in any case below 1) works well for high frequency and/or
-     * large appraisals, so that the effect of these is dampened.
+     * This function returns either the state as is (gain=false) or a state
+     * based on gained limiter (limited between 0 and 1), of which the gain can
+     * be set by using setGain(gain). A high gain factor works well when
+     * appraisals are small and rare, and you want to see the effect of these
+     * appraisals. A low gain factor (close to 0 but in any case below 1) works
+     * well for high frequency and/or large appraisals, so that the effect of
+     * these is dampened.
      *
      * @param gain The gain factor. Leave blank (null) to ignore gain.
      * @return An array of emotions.
@@ -74,12 +74,13 @@ public class AgentInternalState extends ArrayList<Emotion> {
     }
 
     /**
-     * This function prints to the console either the state as is (gain=false) or a state based on
-     * gained limiter (limited between 0 and 1), of which the gain can be set by using setGain(gain).
-     * A high gain factor works well when appraisals are small and rare, and you want to see the
-     * effect of these appraisals
-     * A low gain factor (close to 0 but in any case below 1) works well for high frequency and/or
-     * large appraisals, so that the effect of these is dampened.
+     * This function prints to the console either the state as is (gain=false)
+     * or a state based on gained limiter (limited between 0 and 1), of which
+     * the gain can be set by using setGain(gain). A high gain factor works well
+     * when appraisals are small and rare, and you want to see the effect of
+     * these appraisals A low gain factor (close to 0 but in any case below 1)
+     * works well for high frequency and/or large appraisals, so that the effect
+     * of these is dampened.
      *
      * @param gain The gain factor. Leave blank (null) to ignore gain.
      */
