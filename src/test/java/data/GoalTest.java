c24445b909eaacff5267d9dd7d9cab2e6a211256
nordinvannes <N.vanNes@student.tudelft.nl>
2015-05-13 11:18:49 +0200

    Travis fix?

--- src/test/java/data/GoalTest.java
+++ src/test/java/data/GoalTest.java
@@ -54,4 +54,25 @@ public class GoalTest {
 
   }
 
+  @Test
+  public void testGetters() {
+    assertEquals("TestGoal", goal.getName());
+    assertEquals(.5, goal.getUtility(), 10E-15);
+    assertEquals(false, goal.isMaintenanceGoal());
+    assertEquals(.5, goal.getLikelihood(), 10E-15);
+  }
+
+  @Test
+  public void testSetters() {
+    goal.setName("TestGoalTest");
+    goal.setLikelihood(.2);
+    goal.setUtility(.8);
+    goal.setMaintenanceGoal(true);
+
+    assertEquals("TestGoalTest", goal.getName());
+    assertEquals(.2, goal.getLikelihood(), 10E-15);
+    assertEquals(.8, goal.getUtility(), 10E-15);
+    assertEquals(true, goal.isMaintenanceGoal());
+  }
+
 }
