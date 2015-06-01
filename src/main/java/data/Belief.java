487d4332c02fb45b9a9dd87799ad2da86582c029
svenpopping <s.popping@student.tudelft.nl>
2015-06-01 11:42:49 +0200

    Change the order of imports

--- src/main/java/data/Belief.java
+++ src/main/java/data/Belief.java
@@ -1,10 +1,10 @@
 package data;
 
-import agent.Agent;
-import exception.GoalCongruenceMapException;
-
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import agent.Agent;
+import exception.GoalCongruenceMapException;
 
 /**
  * A Belief contains information about events (Goals) and the amount of positive
