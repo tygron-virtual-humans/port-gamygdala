d44a8392451ab7612273e5021d9248aa961a2ac8
svenpopping <s.popping@student.tudelft.nl>
2015-06-19 12:28:45 +0200

    Change sys.out to Logger in Debug class

--- src/main/java/debug/Debug.java
+++ src/main/java/debug/Debug.java
@@ -1,5 +1,8 @@
 package debug;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Debug class.
  */
@@ -11,13 +14,19 @@ public class Debug {
     private static final boolean DEBUG = true;
 
     /**
+     * Logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(Debug.class.getName());
+
+    /**
      * Print debug information to console if the debug flag is set to true.
      *
-     * @param what Object to print to console.
+     * @param logContent String to be logged.
      */
-    public static void debug(Object what) {
+    public static void debug(String logContent) {
         if (Debug.DEBUG) {
-            System.out.println(what);
+            LOGGER.log(Level.FINE, logContent);
         }
     }
+
 }
